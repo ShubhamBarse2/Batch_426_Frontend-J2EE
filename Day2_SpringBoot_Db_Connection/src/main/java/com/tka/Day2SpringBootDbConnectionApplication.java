3c4f78998582912ef79692f5ce88b32485761ade
ShubhamBarse2 <shubhambarse510@gmail.com>
2026-05-15 12:04:38 +0530

    Add files via upload

---
+++ Day2_SpringBoot_Db_Connection/src/main/java/com/tka/Day2SpringBootDbConnectionApplication.java
@@ -0,0 +1,14 @@
+package com.tka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day2SpringBootDbConnectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day2SpringBootDbConnectionApplication.class, args);
+		System.err.println("Application started ...!");
+	}
+
+}
